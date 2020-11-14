a0731aff3c11be8675d46a72aa8c67a45aea2deb
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-11-14 09:53:01 -0600

    Inicio React App

---
+++ github-jobs/src/GithubJobs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const GithubJobs = () => {
+    return (
+        <div>
+            <h1>GithubJobs</h1>
+        </div>
+    );
+};
---
+++ github-jobs/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GithubJobs } from './GithubJobs';
+
+ReactDOM.render(
+    <GithubJobs />,
+  document.getElementById('root')
+);
